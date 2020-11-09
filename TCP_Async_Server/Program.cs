27ce1a1c1e951ac0e4d423c88d2c59bd41d4ad31
wielas <wielickikrzysiu@gmail.com>
2020-11-09 20:57:41 +0100

    Add project files.

---
+++ TCP_Async_Server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace TCP_Async_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerFactorialAsync server = new ServerFactorialAsync(IPAddress.Any, 2048);
+            server.Start();
+        }
+    }
+}
---
+++ TCP_Async_Server/ServerFactorialAsync.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace TCP_Async_Server
+{
+    class ServerFactorialAsync : ServerFactorial
+    {
+        #region Fields
+
+        private int counter = 0;
+
+        #endregion
+
+        #region Constructors
+
+        public ServerFactorialAsync(IPAddress address, int port) : base(address, port) { }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Initializes asynchronous TCP Server
+        /// </summary>
+        public override void Start()
+        {
+            StartListening();
+            AcceptClient();
+        }
+
+        public string[] ReceivedMessages { get; } = new string[100];
+        public string[] SentMessages { get; } = new string[100];
+
+        /// <summary>
+        /// Listens for incoming clients
+        /// </summary>
+        protected override void AcceptClient()
+        {
+            while (true)
+            {
+                TcpClient tcpClient = TcpListener.AcceptTcpClient();
+
+                Stream = tcpClient.GetStream();
+
+                TransmissionDataDelegate transmissionDelegate = new TransmissionDataDelegate(BeginDataTransmission);
+
+                transmissionDelegate.BeginInvoke(Stream, TransmissionCallback, tcpClient);
+            }
+        }
+
+        private void TransmissionCallback(IAsyncResult asyncResult)
+        {
+            TcpClient = (TcpClient)asyncResult.AsyncState;
+            TcpClient.Close();
+        }
+
+        /// <summary>
+        /// Calculates ammount of each letter in a message
+        /// </summary>
+        /// <param name="message">Message sent by client</param>
+        /// <returns>Count of each letter in meesage</returns>
+        private static string CalculateLetterCount(string message)
+        {
+            message = message.Substring(0, message.IndexOf('\0'));
+
+            var query = message.GroupBy(c => c).Select(c => new { Char = c.Key, Count = c.Count() });
+
+            string response = "";
+
+            foreach (var result in query)
+            {
+                if (result.Count > 0 && result.Count < 100 && result.Char != ' ') response += ("Char " + result.Char + " appears " + result.Count + " times\n");
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Takes incoming messages and returns answer
+        /// </summary>
+        /// <param name="stream">Client stream</param>
+        protected override void BeginDataTransmission(NetworkStream stream)
+        {
+            byte[] buffer = new byte[1024];
+            int number = 0;
+            double result;
+            byte[] sent_back = new byte[1024];
+            byte[] hello = new byte[100];
+            hello = Encoding.Default.GetBytes("Input one or two digit number to calculate factorial\n");
+            stream.Write(hello, 0, hello.Length);
+            while (true)
+            {
+                int msg_len = stream.Read(buffer, 0, 1024);
+                //Console.WriteLine(buffer[0]);
+
+                if (buffer[0] >= 49 && buffer[0] <= 57 && buffer[1] >= 48 && buffer[1] <= 57)
+                {
+                    number = (buffer[0] - 48) * 10 + (buffer[1] - 48);
+                    result = 1;
+                    while (number != 1)
+                    {
+                        result = result * number;
+                        number = number - 1;
+                    }
+                    Console.WriteLine(result);
+                    sent_back = Encoding.Default.GetBytes(result.ToString() + "\n");
+                    stream.Write(sent_back, 0, sent_back.Length);
+                }
+
+                else if (buffer[0] >= 49 && buffer[0] <= 57)
+                {
+                    number = buffer[0] - 48;
+                    result = 1;
+                    while (number != 1)
+                    {
+                        result = result * number;
+                        number = number - 1;
+                    }
+                    Console.WriteLine(result);
+                    sent_back = Encoding.Default.GetBytes(result.ToString() + "\n");
+                    stream.Write(sent_back, 0, sent_back.Length);
+                }
+            }
+        }
+
+        public delegate void TransmissionDataDelegate(NetworkStream stream);
+
+        #endregion
+
+    }
+}
